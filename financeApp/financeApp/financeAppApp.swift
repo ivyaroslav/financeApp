ee89a58f66245b5123d197c4a9244c6c5ffaad40
ivyaroslav <yarikivanitsa39@gmail.com>
2026-09-13 14:01:03 +0100

    Initial commit

---
+++ financeApp/financeApp/financeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  financeAppApp.swift
+//  financeApp
+//
+//  Created by yaroslav on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct financeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
