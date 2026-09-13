67a1ce68d0c0679e94c6232818264ac7cf5e8150
ivyaroslav <yarikivanitsa39@gmail.com>
2026-09-13 18:53:49 +0100

    Added User entity; setting up PersistenceController

---
+++ financeApp/financeApp/PersistenceController.swift
@@ -0,0 +1,40 @@
+//
+//  PersistenceController.swift
+//  financeApp
+//
+//  Created by yaroslav on 13/09/2026.
+//
+
+import Foundation
+import CoreData
+struct PersistenceController {
+    
+    static let shared = PersistenceController()
+    
+    
+    let container: NSPersistentContainer
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "FinanceAppModel")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    func save() {
+        let context = container.viewContext
+
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+            }
+        }
+    }
+}
--- financeApp/financeApp/financeAppApp.swift
+++ financeApp/financeApp/financeAppApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct financeAppApp: App {
+    let persistenceController = PersistenceController.shared
+    @Environment(\.scenePhase) var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+        .onChange(of: scenePhase) { _, newPhase in
+            if newPhase == .background {
+                let context = persistenceController.container.viewContext
+                if context.hasChanges {
+                    try? context.save()
+                }
+            }
         }
     }
+    
 }
