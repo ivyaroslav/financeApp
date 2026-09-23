636dfc5efb19c7d1493ed0a64499c7c4b368450c
ivyaroslav <yarikivanitsa39@gmail.com>
2026-09-23 19:35:18 +0100

    Added testLoadContacts_populatesFromStore to ContactListViewModelTests

--- financeApp/financeAppTests/ViewModels/ContactListViewModelTests.swift
+++ financeApp/financeAppTests/ViewModels/ContactListViewModelTests.swift
@@ -11,7 +11,32 @@ import XCTest
 
 final class ContactListViewModelTests: XCTestCase {
 
-    func testLoadConatcts_populatesFromStore() throws {
-        // not implemented yet
+    func testLoadContacts_populatesFromStore() throws {
+        let store = InMemoryContactStore()
+
+             let contact1 = Contact(
+                 id: UUID(),
+                 firstName: "John",
+                 lastName: "King",
+                 phoneNumber: "+123456789"
+             )
+
+             let contact2 = Contact(
+                 id: UUID(),
+                 firstName: "Sarah",
+                 lastName: "Ivanova",
+                 phoneNumber: "+987654321"
+             )
+
+             try store.save(contact1)
+             try store.save(contact2)
+
+             let viewModel = ContactListViewModel(store: store)
+
+             viewModel.loadContacts()
+
+             XCTAssertEqual(viewModel.contacts.count, 2)
+             XCTAssertEqual(viewModel.contacts[0].id, contact1.id)
+             XCTAssertEqual(viewModel.contacts[1].id, contact2.id)
     }
 }
